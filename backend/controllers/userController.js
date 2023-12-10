464adc636e3fb875a778d4fd5ae64d7ba9207715
Grocky94 <lastleftdefence@gmail.com>
2023-12-10 17:59:37 +0530

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../model/UserModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 // import Quiz from "../model/QuizModel.js";
 
